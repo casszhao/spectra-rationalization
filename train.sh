8dce20bc8162a7f1805a43be201f7e9f717f7578
casszhao <zhixue.zhao@sheffield.ac.uk>
2022-06-01 12:16:06 +0100

    m

--- train.sh
+++ train.sh
@@ -5,8 +5,9 @@ conda activate spectra
 
 
 
-python -m rationalizers train --config configs/cus/seed5.yaml
-python -m rationalizers train --config configs/cus/seed10.yaml
+# python -m rationalizers train --config configs/cus/seed5.yaml
+# python -m rationalizers train --config configs/cus/seed10.yaml
+# exit
 python -m rationalizers train --config configs/cus/seed15.yaml
 python -m rationalizers train --config configs/cus/seed20.yaml
 python -m rationalizers train --config configs/cus/seed25.yaml
